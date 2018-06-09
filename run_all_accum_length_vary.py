9f3e9ce8a68cea425031babde9373490ab999433
matt <mmaierhofer97@gmail.com>
2018-06-09 13:57:21 -0600

    added run

---
+++ run_all_accum_length_vary.py
@@ -0,0 +1,10 @@
+import os
+import sys
+filenames = ["'data/synth_accum/accum_length_vary0'","'data/synth_accum/accum_length_vary1'","'data/synth_accum/accum_length_vary2'","'data/synth_accum/accum_length_vary3'"]
+for i in range(10):
+	print(i)
+	for filename in filenames:
+		cwd = os.path.join(os.getcwd(), "main.py "+filename+" 'LLM' 'GPU'")
+		os.system('{} {}'.format('python3', cwd))
+		cwd = os.path.join(os.getcwd(), "main.py "+filename+" 'LSTM' 'GPU'")
+		os.system('{} {}'.format('python3', cwd))
