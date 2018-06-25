e17753e9ebab63396e74831cfe67e8ddb43b804e
matt <mmaierhofer97@gmail.com>
2018-06-25 15:48:04 -0600

    add Multi

--- Guess_Last.py
+++ Guess_Last.py
@@ -1,10 +1,10 @@
 import csv
 import numpy as np
 ends = ['.train','.test']
-lams = ['0.015625','0.0625','0.25','1','4','16','64']
+lams = ['0.0625','0.25','1','4','16']
 for lam in lams:
     for end in ends:
-        filename ='data/synth_accum/accum_scales'+lam+end
+        filename ='data/synth_accum/accum_multiclass'+lam+end
         print(filename)
         with open(filename,'rt') as csvfile:
             rows=[]
@@ -18,6 +18,6 @@ for lam in lams:
             for i in range(int(len(rows)/4)):
                 tot+=1
                 #print(rows[4*i][-1],rows[4*i+2][-1])
-                if np.sign(int(rows[4*i][-1])-1.5)==int(rows[4*i+2][-1]):
+                if int(rows[4*i][-1])==int(rows[4*i+2][-1]):
                   cor+=1
             print(cor/tot)
---
+++ gen_synth_accum_multiclass.py
@@ -0,0 +1,58 @@
+import random
+import numpy as np
+filepath = 'data/synth_accum/accum_multiclass'
+lams = [1/16,1/4,1,4,16]
+ends = ['.train','.test']
+ev_types = 4
+for lam in lams:
+    for end in ends:
+        print(lam, end)
+        l = 100
+        filename = filepath+str(lam)+end
+        myfile = open(filename,'w')
+        myfile.write('')
+        myfile.close()
+        myfile = open(filename,'a')
+        for count in range(1000):
+            id=str(count+1).zfill(5)
+            #A_timescale = 1
+            #B_timescale = 1
+            events = []
+            for j in range(ev_types):
+                t = 0
+                A_timescale = random.expovariate(1)
+                while len(events)<l*(j+1):
+                    t += random.expovariate(A_timescale)
+                    events.append([j+1,t])
+            events.sort(key=lambda x: x[1])
+            events = events[:l+1]
+            accum = np.zeros(ev_types)
+            accum[events[0][0]-1] = 1
+            time1 = [str(0.0)]
+            time2 = []
+            ordinal = []
+            ordinal2 = []
+            class_id = []
+            for i in range(1,len(events)):
+                ordinal.append(str(events[i-1][0]))
+                delta_t = events[i][1]-events[i-1][1]
+                if i != len(events)-1:
+                    accum = accum * np.exp(lam*-(delta_t))
+                    accum[events[i][0]-1] += 1
+                time1.append(str(delta_t))
+                time2.append(str(delta_t))
+                class_id.append(str(0))
+                ordinal2.append(str(events[i][0]))
+            time1 = time1[:-1]
+            #print(len(time1))
+            class_id[-1] = str(int(np.argmax(accum))+1)
+                #print(class_id[j])
+            #print(len(time1),len(time2),len(ordinal))
+            #print(accum, int(np.sign(accum)),len(events))
+
+            myfile.write(id + ' '+' '.join(ordinal)+'\n')
+            myfile.write(id + ' '+' '.join(time1)+'\n')
+            myfile.write(id + ' '+' '.join(class_id)+'\n')
+            myfile.write(id + ' '+' '.join(time2)+'\n')
+            #myfile.write('foo2')
+        myfile.close()
