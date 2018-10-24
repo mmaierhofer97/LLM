54894f3cf5e58dba671c8253d68e97bc19d8ebc1
matt <mmaierhofer97@gmail.com>
2018-10-24 17:53:06 -0600

    syncination ;

--- Acc_Anal_Paired.py
+++ Acc_Anal_Paired.py
@@ -4,7 +4,7 @@ import sys
 lams = ['']
 
 if len(sys.argv)>1:
-   filename =  sys.argv[1]
+   filename = 'data/'+ sys.argv[1]
 lens = ['']
 
 for i in lens:
@@ -12,7 +12,7 @@ for i in lens:
     rows = []
     for l in lams:
         try:
-            csvfile = open(filename+'.txt','rt')
+            csvfile = open(filename,'rt')
             data = csv.reader(csvfile, delimiter=',')
             for row in data:
                  rows.append(row)
@@ -24,4 +24,10 @@ for i in lens:
     lstm = rows[:,1]
     m=np.average(llm-lstm)
     s=np.sqrt(sum(np.square((llm-lstm)-m))/(len(llm)-1))
+    llm_m=np.average(llm)
+    llm_s=np.sqrt(sum(np.square((llm)-llm_m))/(len(llm)-1))
+    lstm_m=np.average(lstm)
+    lstm_s=np.sqrt(sum(np.square((lstm)-lstm_m))/(len(lstm)-1))
+    print('LLM-{}:  {} +- {}'.format(len(llm),llm_m,llm_s*1.96))
+    print('LSTM-{}:  {} +- {}'.format(len(llm),lstm_m,lstm_s*1.96))
     print('Diff-{}:  {} +- {}'.format(len(llm),m,s*1.96))
