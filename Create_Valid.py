094c34a9b1612a8484a6bfb6205e6b5e2dca7a61
Matt Maierhofer <jema5033@colorado.edu>
2019-01-20 14:14:01 -0700

    v

--- Create_Valid.py
+++ Create_Valid.py
@@ -7,7 +7,9 @@ datasets = ['data/github/github','data/dota/dota','data/dota/dota_class','data/f
 filenames = []
 for ds in datasets:
     for num in ['49','99','199','399']:
+
         filename = ds+'_100_paired_train'+num+'.txt'
+        filename2 = ds+'_100_paired_test'+num+'.txt'
         rows = []
         for l in lams:
             try:
@@ -17,5 +19,14 @@ for ds in datasets:
                      rows.append(row)
             except:
                 0
-        for row in rows:
-            print(row)
+        print(len(rows))
+        rows = []
+        for l in lams:
+            try:
+                csvfile = open(filename2,'rt')
+                data = csv.reader(csvfile, delimiter=',')
+                for row in data:
+                     rows.append(row)
+            except:
+                0
+        print(len(rows))
