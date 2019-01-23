336bc9e357e4969335ebdf47dea9d8b1c3888126
Matt Maierhofer <jema5033@colorado.edu>
2019-01-22 17:39:04 -0700

    v

--- main.py
+++ main.py
@@ -251,8 +251,7 @@ with tf.device(ops['device']):
                         reduction_indices=[1])) / tf.reduce_sum(tf.reduce_sum(P_mask))
 
             # Evaluate the model
-            print(tf.cast(tf.greater(tf.zeros_like(P_y),tf.sign(T_pred*P_y)),tf.float32))
-            T_correct_pred = tf.reduce_sum(tf.greater(tf.sign(T_pred*P_y),tf.zeros_like(P_y)),reduction_indices=[2]) * P_mask
+            T_correct_pred = tf.reduce_sum(tf.cast(tf.greater(tf.zeros_like(P_y),tf.sign(T_pred*P_y)),tf.float32)),reduction_indices=[2]) * P_mask
             T_accuracy = tf.reduce_sum(tf.reduce_sum(tf.cast(T_correct_pred, tf.float32))) / tf.reduce_sum(
                 tf.reduce_sum(P_mask))
     else:
