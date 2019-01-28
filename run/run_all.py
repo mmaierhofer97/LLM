374d7ef47354e9b32c4da83739981de421ba959a
Matt Maierhofer <jema5033@colorado.edu>
2019-01-28 15:53:15 -0700

    v

--- run/run_all.py
+++ run/run_all.py
@@ -5,7 +5,7 @@ args['max_length'] = 'ALL'
 args['task']='PRED'
 args['n_hidden'] = '100'
 args['samples']='ALL'
-args['datasets'] = ['data/github/github','data/dota/dota_class','data/dota/dota','data/freecodecamp_students/freecodecamp_students','data/reddit/reddit','data/reddit_comments/reddit_comments']
+args['datasets'] = ['data/quizlet/quizlet','data/github/github','data/dota/dota_class','data/dota/dota','data/freecodecamp_students/freecodecamp_students','data/reddit/reddit','data/reddit_comments/reddit_comments']
 for st in sys.argv[1:]:
     splt = st.index('=')
     key = st[:splt]
@@ -19,17 +19,25 @@ for ds in args['datasets']:
     argstr = ''
     if 'class' in ds:
         args['task'] = 'CLASS'
+    elif 'quizlet' in ds:
+        args['task'] = 'PRED_CORR'
     else:
         args['task'] = 'PRED'
     for key in args.keys():
         if key != 'datasets':
             argstr+=' '+str(key)+'='+str(args[key])
+    special_case = ''
+    cases = ['github','quizlet']
+    for case in cases:
+        if case in ds:
+        special_case = case
 
-    if ds!='data/github/github':
+    if case!='':
+        cwd = os.path.join(os.getcwd(), "run/run_"+case+".py" +argstr)
+        os.system('{} {}'.format('python3', cwd))
+
+    else:
         argstr += ' dataset='
         argstr+=ds
         cwd = os.path.join(os.getcwd(), "run/run.py" +argstr)
-        os.system('{} {}'.format('python3', cwd))
-    else:
-        cwd = os.path.join(os.getcwd(), "run/run_github.py" +argstr)
         os.system('{} {}'.format('python3', cwd))
