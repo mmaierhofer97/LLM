57e35bc95eb8af5cbdd931ca4314a91791f8e8eb
Matt Maierhofer <jema5033@colorado.edu>
2019-01-30 21:43:37 -0700

    v

--- tensor_classes_helpers.py
+++ tensor_classes_helpers.py
@@ -223,7 +223,7 @@ def calculate_auc(sess, P_x, P_y, P_len, P_mask, P_batch_size, T_auc, T_embeddin
                                                         P_len: batch_maxlen,
                                                         P_mask: mask,
                                                         P_batch_size: batch_size})
-            auc_tot += auc_batch*len(batch_ids)
+            auc_tot += auc_batch[1]*len(batch_ids)
             samples += len(batch_ids)
         auc_entry.append(acc_tot/samples)
     return auc_entry
