11d721c381d75face8295f13dd290a1cc57cd9ad
Matt Maierhofer <jema5033@colorado.edu>
2019-03-18 16:20:26 -0600

    commit

--- Acc_Anal_Paired.py
+++ Acc_Anal_Paired.py
@@ -3,13 +3,13 @@ import csv
 import sys
 from scipy.stats import t
 lams = ['']
-datasets = ['data/github/github','data/dota/dota','data/dota/dota_class','data/freecodecamp_students/freecodecamp_students','data/reddit/reddit','data/reddit_comments/reddit_comments']
+datasets = ['data/github/github','data/dota/dota','data/dota/dota_class','data/freecodecamp_students/freecodecamp_students','data/reddit/reddit','data/reddit_comments/reddit_comments','data/quizlet/quizlet']
 filenames = []
 for ds in datasets:
-    for num in ['100']:#['49','99','199','399','50','100','200','400']:
-        filenames.append(ds+'_100_paired_train'+num+'.txt')
+    for num in ['100','99']:#['49','99','199','399','50','100','200','400']:
+        #filenames.append(ds+'_100_paired_train'+num+'.txt')
         filenames.append(ds+'_100_paired_test'+num+'.txt')
-        filenames.append(ds+'_100_paired_valid'+num+'.txt')
+        #filenames.append(ds+'_100_paired_valid'+num+'.txt')
 if len(sys.argv)>1:
    filenames=[]
    for f in sys.argv[1:]:
