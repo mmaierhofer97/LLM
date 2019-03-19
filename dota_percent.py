4937466e531cbec8a280418cbba85ac7482c3737
Matt Maierhofer <jema5033@colorado.edu>
2019-03-18 21:10:59 -0600

    commit

---
+++ dota_percent.py
@@ -0,0 +1,13 @@
+f = open('data/dota/dota_class.test','r')
+count = 0
+tot = 0
+for st in ['.train','.test']:
+  i = 0
+  for seq in f:
+   i+=1
+   if i%4==3:
+    l = seq[-1]
+    tot += 1
+    if int(l)==1:
+      count += 1
+print(count/tot)
