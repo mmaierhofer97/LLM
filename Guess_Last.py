ed58c50a34785f9e67af10ce42257943a8d6cca6
Matt Maierhofer <jema5033@colorado.edu>
2019-03-18 20:28:11 -0600

    commit

--- Guess_Last.py
+++ Guess_Last.py
@@ -2,13 +2,15 @@ import csv
 import numpy as np
 ends = ['.train','.test']
 lams = ['']
+datasets = ['data/github/github00', 'data/dota/dota','data/freecodecamp_students/freecodecamp_students','data/reddit/reddit','data/reddit_comments/reddit_comments']
 length = 10000
-for end in ends:
+for ds in datasets:
+    end = '.test'
     cor = 0
     tot = 0
-    print(end)
+    print(ds)
     for lam in lams:
-        filename ='data/dota/dota'+end
+        filename =ds+end
         with open(filename,'rt') as csvfile:
             rows=[]
             data = csv.reader(csvfile, delimiter=' ')
@@ -19,7 +21,17 @@ for end in ends:
             for i in range(int(len(rows)/4)):
                 for j in range(min(len(rows[4*i]),length)):
                  #print(rows[4*i][-1],int(float(rows[4*i+2][-1]))
-                 tot += 1 
+                 tot += 1
                  if int(float(rows[4*i][j]))==int(float(rows[4*i+2][j])):
                   cor+=1
-        print(cor/tot)
+        m = (cor/tot)
+        for k in range(max(np.array(rows[0]))):
+            cor = 0
+            tot = 0
+            for i in range(int(len(rows)/4)):
+                for j in range(len(rows[4*i])-3):
+                    tot+=1
+                    if int(rows[4*i][j+1])==k+1:
+                        cor+=1
+            m = max(cor/tot,m)
+        print(m)
