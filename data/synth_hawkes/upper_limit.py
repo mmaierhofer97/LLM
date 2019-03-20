f8c310188561dccd4a9643429724f1fe8e0200f8
Matt Maierhofer <jema5033@colorado.edu>
2019-03-20 15:03:11 -0600

    mess

--- data/synth_hawkes/upper_limit.py
+++ data/synth_hawkes/upper_limit.py
@@ -11,7 +11,7 @@ sys.path.insert(0, '/home/matt/Documents/mozerlab/LLM')
 ev_types=4
 mu = np.repeat(.02,ev_types)
 alph = 0.5
-lens = [10,30,100,400]
+lens = [10,30,100]
 
 if len(sys.argv)>1:
     ev_types = int(sys.argv[1])
@@ -19,7 +19,7 @@ m = 0
 for l in lens:
   cor = 0
   tot = 0
-  for i in range(int(9600/l)):
+  for i in range(int(9000/l)):
     time_scales = []
     for i in range (ev_types):
         time_scales.append(4**i)
@@ -29,12 +29,16 @@ for l in lens:
 
     k = (np.zeros([ev_types,ev_types]))
     for i in range(ev_types):
-            k[i,i-1] = 1/time_scales[i]
-    hawkes = SimuHawkesExpKernels(decays = k, baseline =mu,adjacency = alph*np.ones([ev_types,ev_types]), verbose=False, max_jumps = l+1)
+            k[i,i] = 1/time_scales[i]
+    hawkes = SimuHawkesExpKernels(decays = k, baseline =mu,adjacency = alph*np.ones([ev_types,ev_types]), verbose=False, max_jumps = l)
     dt = 0.01
     hawkes.track_intensity(dt)
     hawkes.simulate()
+    #print(l, 'simulated')
+
     timestamps = hawkes.timestamps
+    if (sum([len(timestamps[i]) for i in range(len(timestamps))]))<l:
+        print(sum([len(timestamps[i]) for i in range(len(timestamps))]))
     intensities = hawkes.tracked_intensity
     intense_times = hawkes.intensity_tracked_times
     intense_times = np.round(intense_times, decimals = 2)
@@ -46,14 +50,18 @@ for l in lens:
     et = events[-1][1]
     times = []
     labels = []
+    #print(timestamps)
+    #print(intense_times)
     for ev in events:
         times.append(ev[1])
         labels.append(ev[0])
-    for i in range(len(times)):
-        t = times[i] - 0.01
+    #print(l, 'evs generated')
+    for i in range(len(times)-1):
+        t = int(times[i]*100)/100
+        #t = np.round(t, decimals = 2)
         m = 0
         for ev in range(ev_types):
-            if(intensities[ev][list(intense_times).index(int(t*100)/100)])>m:
+            if(intensities[ev][list(intense_times).index(t)])>m:
                 m = intensities[ev][list(intense_times).index(int(t*100)/100)]
                 lab = ev+1
         tot += 1
--- most_common.py
+++ most_common.py
@@ -4,7 +4,10 @@ import os
 ends = ['.train','.test']
 cor = 0
 tot = 0
+m = 0
 for i in range(20):
+    cor2 = 0
+    tot2 = 0
     os.system('{} {}'.format('python3', 'gen/gen_synth_accum_pred.py'))
     print(i)
     with open('data/synth_accum/accum_pred100.test','rt') as csvfile:
@@ -17,9 +20,13 @@ for i in range(20):
         print('Got Rows')
         for i in range(int(len(rows)/4)):
             cs = np.zeros(max(rows[4*i].astype(int))+1)
-            for j in range(len(rows[4*i])-3):
-                tot+=1
+            cs[int(rows[4*i][0])]+=1
+            for j in range(len(rows[4*i])-1):
+                tot2+=1
                 if int(rows[4*i][j+1])==np.argmax(cs):
-                    cor+=1
+                    cor2+=1
                 cs[int(rows[4*i][j+1])]+=1
-    print(cor/tot)
+    cor += cor2
+    tot += tot2
+    m = max(m,cor2/tot2)
+    print(m,cor/tot,cor2/tot2)
