b12e2f657011194d11cc3726166d66787d160f4b
Matt Maierhofer <jema5033@colorado.edu>
2019-03-20 11:30:26 -0600

    mess

---
+++ most_common.py
@@ -0,0 +1,21 @@
+import csv
+import numpy as np
+ends = ['.train','.test']
+for end in ends:
+  with open('data/synth_accum/accum_pred100'+end,'rt') as csvfile:
+    rows=[]
+    data = csv.reader(csvfile, delimiter=' ')
+    for row in data:
+        #print(row)
+        rows.append(row)
+    rows = np.array(rows)
+    cor = 0
+    tot = 0
+    for i in range(int(len(rows)/4)):
+        cs = np.zeros(max(int(rows[4*i])))
+        for j in range(len(rows[4*i])-3):
+            tot+=1
+            if int(rows[4*i][j+1])==np.argmax(cs):
+                cor+=1
+            cs[int(rows[4*i][j+1])]+=1
+    print(cor/tot)
