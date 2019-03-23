a3ab389302381fa4851fdcccb06d7659234ade58
Matt Maierhofer <jema5033@colorado.edu>
2019-03-22 18:00:04 -0600

    mess

--- gen/gen_synth_hawkes6.py
+++ gen/gen_synth_hawkes6.py
@@ -1,63 +1,77 @@
-import os
-from inspect import getsourcefile
-import os.path
-import sys
+from numpydoc import docscrape
+from tick.hawkes import SimuHawkesExpKernels, HawkesKernelExp
+from tick.plot import plot_point_process
 
-current_path = os.path.abspath(getsourcefile(lambda:0))
-current_dir = os.path.dirname(current_path)
-parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
 
-sys.path.insert(0, parent_dir)
+#print(intensity)
 
-import sys
+
 import random
-import data_help as DH
-filepath = "data/synth_hawkes/hawkes6"
-filenames = ['100','30','10']
-args = {}
-args['max_length'] = 'ALL'
-args['encoder']='LLM'
-args['device']='GPU'
-args['task']='PRED'
-args['model_load_name']='FALSE'
-args['n_hidden'] = '100'
-for st in sys.argv[1:]:
-    splt = st.index('=')
-    key = st[:splt]
-    val = st[splt+1:]
-    args[key]=val
-for i in range(10):
-	os.system('{} {}'.format('python3', 'gen/gen_synth_hawkes6.py'))
-	print(i)
-	for filename in filenames:
-	    args['dataset'] = filepath+filename
-	    args['seed'] = random.randint(1,10**8)
-	    args['encoder'] = 'LLM'
-	    argstr = ''
-	    for key in args.keys():
-	        argstr+=' '+str(key)+'='+str(args[key])
-	    print(argstr)
-	    cwd = os.path.join(os.getcwd(), "main.py" +argstr)
-	    os.system('{} {}'.format('python3', cwd))
-	    args['encoder'] = 'LSTM'
-	    argstr = ''
-	    for key in args.keys():
-	        argstr+=' '+str(key)+'='+str(args[key])
-	    cwd = os.path.join(os.getcwd(), "main.py"+argstr)
-	    os.system('{} {}'.format('python3', cwd))
-	    LLMfile = open(args['dataset']+'tmp_LLM'+args['n_hidden']+'.txt','rt')
-	    LSTMfile = open(args['dataset']+'tmp_LSTM'+args['n_hidden']+'.txt','rt')
-	    a = [float(i) for i in LLMfile.read()[:-2].split(',')]
-	    b =  [float(i) for i in LSTMfile.read()[:-2].split(',')]
-	    ml = str(args['max_length'])
-	    DH.write_history([a[1],b[1]],args['dataset']+'_'+str(ml)+'_paired_test'+args['n_hidden']+'.txt', i, False)
-	    DH.write_history([a[0],b[0]],args['dataset']+'_'+str(ml)+'_paired_train'+args['n_hidden']+'.txt', i, False)
-	    DH.write_history([a[2],b[2]],args['dataset']+'_'+str(ml)+'_paired_valid'+args['n_hidden']+'.txt', i, False)
-	    LLMfile = open(args['dataset']+'tmp_auc_LLM'+args['n_hidden']+'.txt','rt')
-	    LSTMfile = open(args['dataset']+'tmp_auc_LSTM'+args['n_hidden']+'.txt','rt')
-	    a = [float(i) for i in LLMfile.read()[:-2].split(',')]
-	    b =  [float(i) for i in LSTMfile.read()[:-2].split(',')]
-	    ml = str(args['max_length'])
-	    DH.write_history([a[1],b[1]],args['dataset']+'_'+str(ml)+'_paired_test_auc'+args['n_hidden']+'.txt', i, False)
-	    DH.write_history([a[0],b[0]],args['dataset']+'_'+str(ml)+'_paired_train_auc'+args['n_hidden']+'.txt', i, False)
-	    DH.write_history([a[2],b[2]],args['dataset']+'_'+str(ml)+'_paired_valid_auc'+args['n_hidden']+'.txt', i, False)
+import numpy as np
+import sys
+filepath = 'data/synth_hawkes/hawkes6'
+lens = [10,30,100]
+if len(sys.argv)>1:
+    lens = [int(sys.argv[1])]
+ends = ['.train','.test']
+ev_types = 4
+time_scales = []
+mu = np.repeat(.02,ev_types)
+alph = 0.5
+for l in lens:
+    for end in ends:
+        countall = 0
+        print(l, end)
+        filename = filepath+str(l)+end
+        myfile = open(filename,'w')
+        myfile.write('')
+        myfile.close()
+        myfile = open(filename,'a')
+        for count in range(1000):
+            for i in range (ev_types):
+                time_scales.append(4**i)
+            countall += 1
+            id=str(countall).zfill(5)
+
+            events = []
+            t = 0
+
+            k = (np.zeros([ev_types,ev_types]))
+            for i in range(ev_types):
+                k[i,i-1] = 1/time_scales[i]
+            hawkes = SimuHawkesExpKernels(decays = k, baseline =mu,adjacency = alph*np.ones([ev_types,ev_types]), verbose=False, max_jumps = l+1)
+            dt = 0.01
+            hawkes.track_intensity(dt)
+            hawkes.simulate()
+            timestamps = hawkes.timestamps
+            intensities = hawkes.tracked_intensity
+            intense_times = hawkes.intensity_tracked_times
+            for ev in range(ev_types):
+                for t in timestamps[ev]:
+                    events.append([ev+1,t])
+            events.sort(key=lambda x: x[1])
+            time1 = [str(0.0)]
+            time2 = []
+            ordinal = []
+            ordinal2 = []
+            class_id = []
+            for i in range(1,len(events)):
+                ordinal.append(str(events[i-1][0]))
+                delta_t = events[i][1]-events[i-1][1]
+                time1.append(str(delta_t))
+                time2.append(str(delta_t))
+                class_id.append(str(0))
+                ordinal2.append(str(events[i][0]))
+            time1 = time1[:-1]
+            #print(len(time1))
+            class_id[-1] = str(1)
+                #print(class_id[j])
+            #print(len(time1),len(time2),len(ordinal))
+            #print(accum, int(np.sign(accum)),len(events))
+            myfile.write(id + ' '+' '.join(ordinal)+'\n')
+            myfile.write(id + ' '+' '.join(time1)+'\n')
+            myfile.write(id + ' '+' '.join(ordinal2)+'\n')
+            myfile.write(id + ' '+' '.join(time2)+'\n')
+
+                #myfile.write('foo2')
+        myfile.close()
--- run/run_hawkes6.py
+++ run/run_hawkes6.py
@@ -1,14 +1,63 @@
 import os
+from inspect import getsourcefile
+import os.path
 import sys
-filepath = "'data/synth_hawkes/hawkes6"
-filename = "100"
-if len(sys.argv)>1:
-	filename = sys.argv[1]
+
+current_path = os.path.abspath(getsourcefile(lambda:0))
+current_dir = os.path.dirname(current_path)
+parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
+
+sys.path.insert(0, parent_dir)
+
+import sys
+import random
+import data_help as DH
+filepath = "data/synth_hawkes/hawkes6"
+filenames = ['100','30','10']
+args = {}
+args['max_length'] = 'ALL'
+args['encoder']='LLM'
+args['device']='GPU'
+args['task']='PRED'
+args['model_load_name']='FALSE'
+args['n_hidden'] = '100'
+for st in sys.argv[1:]:
+    splt = st.index('=')
+    key = st[:splt]
+    val = st[splt+1:]
+    args[key]=val
 for i in range(10):
-	cwd = os.path.join(os.getcwd(), "gen_synth_hawkes6.py "+filename)
-	os.system('{} {}'.format('python3', cwd))
+	os.system('{} {}'.format('python3', 'gen/gen_synth_hawkes6.py'))
 	print(i)
-	cwd = os.path.join(os.getcwd(), "main.py dataset="+filepath+filename+"' 'encoder=LLM' 'device=GPU' 'task=PRED' 'model_load_name=FALSE'"+args)
-	os.system('{} {}'.format('python3', cwd))
-	cwd = os.path.join(os.getcwd(), "main.py dataset="+filepath+filename+"' 'encoder=LSTM' 'device=GPU' 'task=PRED' 'model_load_name=FALSE'"+args)
-	os.system('{} {}'.format('python3', cwd))
+	for filename in filenames:
+	    args['dataset'] = filepath+filename
+	    args['seed'] = random.randint(1,10**8)
+	    args['encoder'] = 'LLM'
+	    argstr = ''
+	    for key in args.keys():
+	        argstr+=' '+str(key)+'='+str(args[key])
+	    print(argstr)
+	    cwd = os.path.join(os.getcwd(), "main.py" +argstr)
+	    os.system('{} {}'.format('python3', cwd))
+	    args['encoder'] = 'LSTM'
+	    argstr = ''
+	    for key in args.keys():
+	        argstr+=' '+str(key)+'='+str(args[key])
+	    cwd = os.path.join(os.getcwd(), "main.py"+argstr)
+	    os.system('{} {}'.format('python3', cwd))
+	    LLMfile = open(args['dataset']+'tmp_LLM'+args['n_hidden']+'.txt','rt')
+	    LSTMfile = open(args['dataset']+'tmp_LSTM'+args['n_hidden']+'.txt','rt')
+	    a = [float(i) for i in LLMfile.read()[:-2].split(',')]
+	    b =  [float(i) for i in LSTMfile.read()[:-2].split(',')]
+	    ml = str(args['max_length'])
+	    DH.write_history([a[1],b[1]],args['dataset']+'_'+str(ml)+'_paired_test'+args['n_hidden']+'.txt', i, False)
+	    DH.write_history([a[0],b[0]],args['dataset']+'_'+str(ml)+'_paired_train'+args['n_hidden']+'.txt', i, False)
+	    DH.write_history([a[2],b[2]],args['dataset']+'_'+str(ml)+'_paired_valid'+args['n_hidden']+'.txt', i, False)
+	    LLMfile = open(args['dataset']+'tmp_auc_LLM'+args['n_hidden']+'.txt','rt')
+	    LSTMfile = open(args['dataset']+'tmp_auc_LSTM'+args['n_hidden']+'.txt','rt')
+	    a = [float(i) for i in LLMfile.read()[:-2].split(',')]
+	    b =  [float(i) for i in LSTMfile.read()[:-2].split(',')]
+	    ml = str(args['max_length'])
+	    DH.write_history([a[1],b[1]],args['dataset']+'_'+str(ml)+'_paired_test_auc'+args['n_hidden']+'.txt', i, False)
+	    DH.write_history([a[0],b[0]],args['dataset']+'_'+str(ml)+'_paired_train_auc'+args['n_hidden']+'.txt', i, False)
+	    DH.write_history([a[2],b[2]],args['dataset']+'_'+str(ml)+'_paired_valid_auc'+args['n_hidden']+'.txt', i, False)
