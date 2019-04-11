54b844fb1e54ee3edff6ee8e1278f623db3299f8
Matt Maierhofer <jema5033@colorado.edu>
2019-04-11 14:16:34 -0600

    mess

--- run/run.py
+++ run/run.py
@@ -28,7 +28,7 @@ for st in sys.argv[1:]:
 for i in range(10):
     print(i)
     args['seed'] = random.randint(1,10**8)
-    args['encoder'] = 'LLM'
+    args['encoder'] = 'LLM2'
     argstr = ''
     for key in args.keys():
         argstr+=' '+str(key)+'='+str(args[key])
